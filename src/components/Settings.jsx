ce05875d21a46772e0dd3b9fbd8cbe70ec3b8ff1
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-02-21 22:28:13 +0100

    Settings section structure update

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,28 +3,81 @@ import PropTypes from 'prop-types';
 import '../assets/Settings.css';
 
 function Settings({toggle}){
+
+  const theme = toggle ? "light-mode" : "dark-mode";
   
     return (
-      <div className={`Section-container ${toggle?"light-mode":"dark-mode"}`}>
-        <div className={`Settings-header ${toggle?"light-mode-header":"dark-mode-header"}`}>
+      <div className={`Section-container ${theme}`}>
+        <div className={`Settings-header ${theme + "-header"}`}>
           <h3 className="Settings-title">Settings</h3>
         </div>
-        <h3 className=''>Notifications Settings</h3>
-        <div className='Notifications-tab'>Email Notifications</div>
-        <div className='Notifications-tab'>Push Notifications</div>
-        <hr />
-        <h3 className=''>Security</h3>
-        <div className='Notifications-tab'>Email Notifications</div>
-        <div className='Notifications-tab'>Push Notifications</div>
-        <hr />
-        <h3 className=''>Unusual Activity Settings</h3>
-        <div className='Notifications-tab'>Email Notifications</div>
-        <div className='Notifications-tab'>Push Notifications</div>
-        <hr />
-        <h3 className=''>Alerts Settings</h3>
-        <div className='Notifications-tab'>Email Notifications</div>
-        <div className='Notifications-tab'>Push Notifications</div>
-        <hr />
+        <div className='Settings-tab-groups-container'>
+
+          <div className='Settings-tab-group'>
+            <h3 className='Settings-tab-group-header'>Notifications Settings</h3>
+            <div className='Settings-tab-group-tabs'>
+              <div>
+                <input type="checkbox" id='email-notifications' className='Notifications-tab'/>
+                <label htmlFor='email-notifications'>Email Notifications</label>
+              </div>
+              <div>
+                <input type="checkbox" id='push-notifications' className='Notifications-tab'/>
+                <label htmlFor='push-notifications'>Push Notifications</label>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className='Settings-tab-group'>
+            <h3 className='Settings-tab-group-header'>Security</h3>
+            <div className='Settings-tab-group-tabs'>
+              <div>
+                <input type="checkbox" id='email-notifications' className='Notifications-tab'/>
+                <label htmlFor='email-notifications'>Email Notifications</label>
+              </div>
+              <div>
+                <input type="checkbox" id='push-notifications' className='Notifications-tab'/>
+                <label htmlFor='push-notifications'>Push Notifications</label>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className='Settings-tab-group'>
+            <h3 className='Settings-tab-group-header'>Unusual Activity Settings</h3>
+            <div className='Settings-tab-group-tabs'>
+              <div>
+                <input type="checkbox" id='email-notifications' className='Notifications-tab'/>
+                <label htmlFor='email-notifications'>Email Notifications</label>
+              </div>
+              <div>
+                <input type="checkbox" id='push-notifications' className='Notifications-tab'/>
+                <label htmlFor='push-notifications'>Push Notifications</label>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className='Settings-tab-group'>
+            <h3 className='Settings-tab-group-header'>Alerts Settings</h3>
+            <div className='Settings-tab-group-tabs'>
+              <div>
+                <input type="checkbox" id='email-notifications' className='Notifications-tab'/>
+                <label htmlFor='email-notifications'>Email Notifications</label>
+              </div>
+              <div>
+                <input type="checkbox" id='push-notifications' className='Notifications-tab'/>
+                <label htmlFor='push-notifications'>Push Notifications</label>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+        </div>
       </div>
     );
   }
