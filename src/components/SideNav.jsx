1889808159e394be184ebae68eb72e90c870d11d
Dylan <dylan.imail@gmail.com>
2023-02-20 19:41:37 -0500

    issue #60 - Added MUI icons to SideNav component

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 //import { NavLink as Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import '../assets/SideNav.css';
+import GroupsIcon from '@mui/icons-material/Groups';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 
 class SideNav extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -12,37 +14,22 @@ class SideNav extends React.Component {
     };
   }
 
-  /* openNav = () => {
-      document.getElementById('Side-navigation').style.width = '250px';
-      document.getElementById('main').style.left = '250px';
-    }
-  };*/
-  
-  /* closeNav = () => {
-    document.getElementById('Side-navigation').style.width = '0';
-    document.getElementById('main').style.left = '0';
-  };
-
-  /* const [show, setShow] = useState(); */
-
   render() {
     return (
       <Menu>
-          <a className="menu-item" href="/groups">
+        <a className="menu-item" href="/groups">
+          <GroupsIcon className="side-nav-icon" />
           Groups 
-          </a>
-          <a className="menu-item" href="/events">
-          Event
         </a>
-
+        <a className="menu-item" href="/events">
+          <CalendarMonthIcon className="side-nav-icon" />
+          Events
+        </a>
         <a className="menu-item" href="/friends">
+        <PeopleAltIcon className="side-nav-icon" />
           Friends
         </a>
-        
-      
       </Menu>
-
-      
     );
   }
 }
