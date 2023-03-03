3959cd3dedc2bd748d1e9f045c232c5f7ff16eca
Manuel <manuelpinedacabeza@gmail.com>
2023-03-03 11:16:08 +0100

    Added NotificationBell component

---
+++ src/components/NotificationBell.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+const NotificationBell = ({ notifications, setOpen }) => {
+  const newNotifications = `You have ${notifications.length} notifications!`;
+  const noNewNotifications = "No new notifications";
+
+  const handleOpen = (event) => {
+    console.log("Notication menu opened!");
+    setOpen && setOpen(true);
+  };
+
+  return (
+    <Tooltip
+      title={notifications.length ? newNotifications : noNewNotifications}
+    >
+      <IconButton
+        size="large"
+        color="inherit"
+        aria-label={`show ${notifications.length} new notifications`}
+        onClick={notifications.length ? handleOpen : null}
+      >
+        <Badge badgeContent={notifications.length} color="error">
+          <NotificationsIcon color="inherit" />
+        </Badge>
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default NotificationBell;
+
+NotificationBell.propTypes = {
+  notifications: PropTypes.array,
+  setOpen: PropTypes.func,
+  iconColor: PropTypes.string,
+};
