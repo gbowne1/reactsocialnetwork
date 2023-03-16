010c87c7c3e552dd5e3400e86ca91316d81454a0
Manuel <manuelpinedacabeza@gmail.com>
2023-03-16 11:07:07 +0100

    Added data folder

---
+++ src/data/eventsData.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    title: "Cinema Night!",
+    locationName: "Cinema32",
+    locationUrl: "#",
+    imageUrl:
+      "https://www.shutterstock.com/image-photo/02-august-2018bucharest-romania-people-260nw-1148998826.jpg",
+    attendance: null,
+
+    participation: {
+      interested: 46,
+      going: 27,
+    },
+  },
+  {
+    title: "Pub Crawl",
+    locationName: "Bulldog Bar",
+    locationUrl: "#",
+    imageUrl:
+      "https://www.shutterstock.com/image-photo/happy-friends-cheering-drinking-cocktails-260nw-1109615582.jpg",
+    attendance: null,
+    participation: {
+      interested: 52,
+      going: 38,
+    },
+  },
+  {
+    title: "Mini golf!",
+    locationName: "Mini golf park",
+    locationUrl: "#",
+    imageUrl:
+      "https://www.shutterstock.com/image-photo/group-smiling-friends-enjoying-together-260nw-1814772797.jpg",
+    attendance: null,
+    participation: {
+      interested: 106,
+      going: 78,
+    },
+  },
+];
---
+++ src/data/placeholderImageUrl.js
@@ -0,0 +1,1 @@
+export default "https://wiki.tripwireinteractive.com/images/4/47/Placeholder.png";
