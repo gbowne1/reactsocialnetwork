255183bcb03582bbc2d63e794148ca800af427c1
Manuel <manuelpinedacabeza@gmail.com>
2023-03-16 11:14:12 +0100

    Imported day adapter, localization provider and datepicker.
    Added eventLocationUrl state.
    Renamed handlCreateEvent to handlCreateEventClicked.
    Added default attendance value for new events to "Going".
    
    Renamed setButtonEnabled to setCreateEventButtonEnabled.
    
    Added eventLocationUrl.length to checks.
    Added datepicker to modal inputs.

--- src/components/CreateEventModal.jsx
+++ src/components/CreateEventModal.jsx
@@ -5,30 +5,30 @@ import { Button, Stack, TextField } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-// import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from "@mui/material/DialogTitle";
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 import "../assets/CreateEventModal.css";
 
-const CreateEventModal = ({
-  isOpen,
-  handleClose,
-  events,
-  setEvents,
-}) => {
+const CreateEventModal = ({ isOpen, handleClose, events, setEvents }) => {
   const [eventTitle, setEventTitle] = useState("");
   const [eventLocation, setEventLocation] = useState("");
+  const [eventLocationUrl, setEventLocationUrl] = useState("");
   const [eventImageUrl, setEventImageUrl] = useState("");
 
-  const [buttonEnabled, setButtonEnabled] = useState(false);
+  const [createEventButtonEnabled, setCreateEventButtonEnabled] =
+    useState(false);
 
-  const handlCreateEvent = () => {
+  const handlCreateEventClicked = () => {
     const newEvent = {
       title: eventTitle,
       locationName: eventLocation,
       locationUrl: "#",
       imageUrl: eventImageUrl,
-      attendance: null,
+      attendance: "Going",
 
       participation: {
         interested: 1,
@@ -41,71 +41,86 @@ const CreateEventModal = ({
   };
 
   useEffect(() => {
-    setButtonEnabled(
+    setCreateEventButtonEnabled(
       eventTitle.length > 0 &&
         eventLocation.length > 0 &&
+        eventLocationUrl.length > 0 &&
         eventImageUrl.length > 0
     );
   }, [eventTitle, eventLocation, eventImageUrl]);
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={handleClose}
-      className={`CreateEventModal`}
-    >
-      <DialogTitle className={`CreateEventModal__title`}>
-        Create Event
-      </DialogTitle>
-      <DialogContent className={`CreateEventModal__content`}>
-        Fill in this form to create a new event.
-      </DialogContent>
-      <Stack spacing={2} className={`CreateEventModal__inputs`}>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="event-name"
-          label="Event name"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={eventTitle}
-          onChange={(event) => setEventTitle(event.target.value)}
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="event-location"
-          label="Event location"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={eventLocation}
-          onChange={(event) => setEventLocation(event.target.value)}
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="event-image-url"
-          label="Event image url"
-          type="url"
-          fullWidth
-          variant="standard"
-          value={eventImageUrl}
-          onChange={(event) => setEventImageUrl(event.target.value)}
-        />
-      </Stack>
-      <DialogActions>
-        <Button
-          className={`CreateEventModal__button`}
-          variant="contained"
-          onClick={handlCreateEvent}
-          disabled={!buttonEnabled}
-        >
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        className={`CreateEventModal`}
+      >
+        <DialogTitle className={`CreateEventModal__title`}>
           Create Event
-        </Button>
-      </DialogActions>
-    </Dialog>
+        </DialogTitle>
+        <DialogContent className={`CreateEventModal__content`}>
+          Fill in this form to create a new event.
+        </DialogContent>
+        <Stack spacing={2} className={`CreateEventModal__inputs`}>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="event-name"
+            label="Event name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={eventTitle}
+            onChange={(event) => setEventTitle(event.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="event-location"
+            label="Event location"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={eventLocation}
+            onChange={(event) => setEventLocation(event.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="event-location-url"
+            label="Event location url"
+            type="url"
+            fullWidth
+            variant="standard"
+            value={eventLocationUrl}
+            onChange={(event) => setEventLocationUrl(event.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="event-image-url"
+            label="Event image url"
+            type="url"
+            fullWidth
+            variant="standard"
+            value={eventImageUrl}
+            onChange={(event) => setEventImageUrl(event.target.value)}
+          />
+          <DatePicker />
+        </Stack>
+        <DialogActions>
+          <Button
+            className={`CreateEventModal__button`}
+            variant="contained"
+            onClick={handlCreateEventClicked}
+            disabled={!createEventButtonEnabled}
+          >
+            Create Event
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </LocalizationProvider>
   );
 };
 
