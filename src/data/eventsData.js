33e744c765f5608edaf01c1651b8f2164f158cc0
Manuel <manuelpinedacabeza@gmail.com>
2023-03-19 18:51:08 +0100

    Added attendance as Not Going by default

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -6,7 +6,7 @@ export default [
     locationUrl: "#",
     imageUrl:
       "https://www.shutterstock.com/image-photo/02-august-2018bucharest-romania-people-260nw-1148998826.jpg",
-    attendance: null,
+    attendance: "Not Going",
 
     participation: {
       interested: 46,
@@ -20,7 +20,7 @@ export default [
     locationUrl: "#",
     imageUrl:
       "https://www.shutterstock.com/image-photo/happy-friends-cheering-drinking-cocktails-260nw-1109615582.jpg",
-    attendance: null,
+    attendance: "Not Going",
     participation: {
       interested: 52,
       going: 38,
@@ -33,7 +33,7 @@ export default [
     locationUrl: "#",
     imageUrl:
       "https://www.shutterstock.com/image-photo/group-smiling-friends-enjoying-together-260nw-1814772797.jpg",
-    attendance: null,
+    attendance: "Not Going",
     participation: {
       interested: 106,
       going: 78,
