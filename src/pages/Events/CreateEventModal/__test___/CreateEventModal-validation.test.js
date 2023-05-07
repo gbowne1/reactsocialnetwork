4111cfcb9684d7b0b84eee4bc406aa0c1159d19a
Manuel <manuelpinedacabeza@gmail.com>
2023-05-07 18:17:22 +0200

    Added element constants

--- src/pages/Events/CreateEventModal/__test___/CreateEventModal-validation.test.js
+++ src/pages/Events/CreateEventModal/__test___/CreateEventModal-validation.test.js
@@ -7,7 +7,9 @@ describe("Test CreateEventModal validation", () => {
   it("should display error labels when leaving required fields empty", async () => {
     renderCreateEventModal();
 
-    fireEvent.click(screen.getByTestId("create-event-button"));
+    const createEventButton = screen.getByTestId("create-event-button");
+    fireEvent.click(createEventButton);
+
     const eventTitleHelperText = await screen.findByText(
       "Event title is required!"
     );
@@ -35,7 +37,9 @@ describe("Test CreateEventModal validation", () => {
     await act(async () => {
       await user.type(eventTitleInput, "ABCDE");
       expect(eventTitleInput.value).toBe("ABCDE");
-      await user.click(screen.getByTestId("create-event-button"));
+
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
     const helperText = document.querySelector("#event-title-input-helper-text");
     expect(helperText).toHaveTextContent(
@@ -51,7 +55,9 @@ describe("Test CreateEventModal validation", () => {
     await act(async () => {
       await user.type(eventLocationInput, "ABCDE");
       expect(eventLocationInput.value).toBe("ABCDE");
-      await user.click(screen.getByTestId("create-event-button"));
+
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
     const helperText = document.querySelector(
       "#event-location-input-helper-text"
@@ -71,7 +77,9 @@ describe("Test CreateEventModal validation", () => {
     await act(async () => {
       await user.type(eventLocationUrlInput, "notAValidUrl");
       expect(eventLocationUrlInput.value).toBe("notAValidUrl");
-      await user.click(screen.getByTestId("create-event-button"));
+
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
     const helperText = document.querySelector(
       "#event-location-url-input-helper-text"
@@ -87,7 +95,9 @@ describe("Test CreateEventModal validation", () => {
     await act(async () => {
       await user.type(eventImageUrlInput, "notAValidUrl");
       expect(eventImageUrlInput.value).toBe("notAValidUrl");
-      await user.click(screen.getByTestId("create-event-button"));
+
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
     const helperText = document.querySelector(
       "#event-image-url-input-helper-text"
@@ -102,7 +112,8 @@ describe("Test CreateEventModal validation", () => {
     fireEvent.change(eventDateInput, { target: { value: "04/24/2022" } });
 
     await act(async () => {
-      await user.click(screen.getByTestId("create-event-button"));
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
 
     const helperText = screen.getByText(/Date cannot be in the past!/i);
@@ -116,7 +127,8 @@ describe("Test CreateEventModal validation", () => {
     fireEvent.change(eventDateInput, { target: { value: "" } });
 
     await act(async () => {
-      await user.click(screen.getByTestId("create-event-button"));
+      const createEventButton = screen.getByTestId("create-event-button");
+      await user.click(createEventButton);
     });
 
     const helperText = screen.getByText(/Invalid Date/i);
