93ad02256af2dc10c273d3341a921bbf6c55deeb
Manuel <manuelpinedacabeza@gmail.com>
2023-05-09 20:57:56 +0200

    Added loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import { CircularProgress } from "@mui/material";
+import "./LoadingSpinner.css";
+
+
+const LoadingSpinner = () => {
+  return (
+    <div data-testid="loading-spinner" className={"LoadingSpinner"}>
+      <CircularProgress size={60} />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
