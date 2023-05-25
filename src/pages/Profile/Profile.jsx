a3e523f0e454c6e4385074a5a20fc1eae8536d09
Promise Dash <promisedash1607@gmail.com>
2023-05-25 13:31:52 +0530

    Resolve conflits

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import './Profile.css';
 
 const Profile = () => {
-
-  //All the profile information that the use provides is collected in formData
   const [formData, setFormData] = useState({});
   const [profileImage, setProfileImage] = useState(null);
 
@@ -17,8 +15,6 @@ const Profile = () => {
         setProfileImage(img);
     }
   };
-
-  // form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     let UserData = formData;
@@ -32,132 +28,122 @@ const Profile = () => {
     }
   }
 
-
   return (
     <div className='profile'>
+      <div className="Profile-container">
 
-        <div className="Profile-container">
+      <button className="Close-button">
+        <span className='icon'>&times;</span>
+      </button>
 
-          <button className="Close-button">
-            <span className='icon'>&times;</span>
-          </button>
+      <form className="infoForm" onSubmit={handleSubmit}>
+        <h3>Create Profile</h3>
 
-          <form className="infoForm" onSubmit={handleSubmit}>
-            <h3>Create Profile</h3>
-
-            <div>
-              Profile image
-              <input type="file" name="profileImage" onChange={onImageChange} />
-            </div>
-
-            <div>
-              <input
-                value={formData.firstname}
-                onChange={handleChange}
-                type="text"
-                placeholder="First Name"
-                name="firstname"
-                className="infoInput"
-              />
-              <input
-                value={formData.lastname}
-                onChange={handleChange}
-                type="text"
-                placeholder="Last Name"
-                name="lastname"
-                className="infoInput"
-              />
-            </div>
-
-            <div>
-              <input
-                  value={formData.username}
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                  className="infoInput"
-                />
-            </div>
-
-            <div>
-              <input
-                value={formData.location}
-                onChange={handleChange}
-                type="text"
-                placeholder="Location"
-                name="location"
-                className="infoInput"
-              />
-              <input
-                value={formData.relationship}
-                onChange={handleChange}
-                type="text"
-                className="infoInput"
-                placeholder="Relationship status"
-                name="relationship"
-              />
-            </div>
-
-            <textarea
-                style={{width: '90%'}} 
-                rows={5}
-                value={formData.bio}
-                onChange={handleChange}
-                type="text"
-                className="infoInput"
-                placeholder="Bio"
-                name="bio"
-              />
-
-            <div>
-              <input
-                  value={formData.website}
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Website"
-                  name="website"
-                  className="infoInput"
-                />
-            </div>
-
-            <div>
-              <input
-                  value={formData.interest}
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Interest"
-                  name="interest"
-                  className="infoInput"
-                />
-            </div>
-
-            <div>
-                <input
-                  value={formData.privarySetting}
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Privary Setting (public/private/friends only)"
-                  name="privarySetting"
-                  className="infoInput"
-                />
-
-              <input
-                  value={formData.notificationSetting}
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Notifictaion Setting (email/push notification)"
-                  name="notificationSetting"
-                  className="infoInput"
-                />
-            </div>
-
-            <button className="infoButton" type="submit">
-              Create
-            </button>
-
-          </form>
+        <div>
+          Profile image
+          <input type="file" name="profileImage" onChange={onImageChange} />
         </div>
+        <div>
+          <input
+            value={formData.firstname}
+            onChange={handleChange}
+            type="text"
+            placeholder="First Name"
+            name="firstname"
+            className="infoInput"
+          />
+          <input
+            value={formData.lastname}
+            onChange={handleChange}
+            type="text"
+            placeholder="Last Name"
+            name="lastname"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+              value={formData.username}
+              onChange={handleChange}
+              type="text"
+              placeholder="Username"
+              name="username"
+              className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            value={formData.location}
+            onChange={handleChange}
+            type="text"
+            placeholder="Location"
+            name="location"
+            className="infoInput"
+          />
+          <input
+            value={formData.relationship}
+            onChange={handleChange}
+            type="text"
+            className="infoInput"
+            placeholder="Relationship status"
+            name="relationship"
+          />
+        </div>
+        <textarea
+          style={{width: '90%'}} 
+          rows={5}
+          value={formData.bio}
+          onChange={handleChange}
+          type="text"
+          className="infoInput"
+          placeholder="Bio"
+          name="bio"
+        />
+        <div>
+          <input
+            value={formData.website}
+            onChange={handleChange}
+            type="text"
+            placeholder="Website"
+            name="website"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            value={formData.interest}
+            onChange={handleChange}
+            type="text"
+            placeholder="Interest"
+            name="interest"
+            className="infoInput"
+          />
+        </div>
+        <div>
+          <input
+            value={formData.privarySetting}
+            onChange={handleChange}
+            type="text"
+            placeholder="Privary Setting (public/private/friends only)"
+            name="privarySetting"
+            className="infoInput"
+          />
+          <input
+            value={formData.notificationSetting}
+            onChange={handleChange}
+            type="text"
+            placeholder="Notifictaion Setting (email/push notification)"
+            name="notificationSetting"
+            className="infoInput"
+          />
+        </div>
+
+        <button className="infoButton" type="submit">
+          Create
+        </button>
+
+        </form>
+      </div>
     </div>
   );
 };
