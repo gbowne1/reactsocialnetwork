2a2c1fc11b7bd2fbb909038fff6cc9e6e92a1b35
Greg Bowne <gbowne144@gmail.com>
2023-06-20 00:40:44 -0700

    Initial commit of AdminDashboard change
    - Adding AdminDashboard to SideNav
    - Adding Panel.jsx custom component
    - Adding route to App.js
    - Ammending styles

--- client/src/layouts/SideNav/SideNav.jsx
+++ client/src/layouts/SideNav/SideNav.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import GroupsIcon from "@mui/icons-material/Groups";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 const SideNav = ({ isSideNavVisible, themeMode }) => {
   return (
@@ -24,6 +25,10 @@ const SideNav = ({ isSideNavVisible, themeMode }) => {
       <a className={`menu-item ${themeMode}`} href="/friends">
         <PeopleAltIcon className="side-nav-icon" />
         Friends
+      </a>
+      <a className={`menu-item ${themeMode}`} href="/admin">
+        <AdminPanelSettingsIcon className="side-nav-icon" />
+        Admin
       </a>
     </Menu>
   );
