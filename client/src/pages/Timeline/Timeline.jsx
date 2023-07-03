48defa7bc0d011a9044cb349bbc4ed773264b9b8
Manuel <manuelpinedacabeza@gmail.com>
2023-07-03 11:19:26 +0200

    Added themeMode and pass it on to classes

--- client/src/pages/Timeline/Timeline.jsx
+++ client/src/pages/Timeline/Timeline.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Post from "../../components/Post/Post";
 import PostForm from "../../components/PostForm/PostForm";
-import postsData from "../../data/postsData";
 import "./Timeline.css";
 
 const Timeline = ({ themeMode }) => {
@@ -23,10 +22,10 @@ const Timeline = ({ themeMode }) => {
   }, [posts, shownPosts]);
 
   return (
-    <div className="timeline" data-testid="timeline">
+    <div className={`Timeline ${themeMode}`} data-testid="timeline">
       <PostForm themeMode={themeMode} posts={posts} setPosts={setPosts} />
       {shownPosts.map((post, i) => (
-        <Post key={i} {...post} />
+        <Post key={i} themeMode={themeMode} {...post} />
       ))}
     </div>
   );
