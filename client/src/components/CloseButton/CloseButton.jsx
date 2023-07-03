c3d651b6f2b1bb3d2757b709673f4d113b5cf826
Manuel <manuelpinedacabeza@gmail.com>
2023-07-03 11:16:01 +0200

    Added className prop to button

--- client/src/components/CloseButton/CloseButton.jsx
+++ client/src/components/CloseButton/CloseButton.jsx
@@ -2,9 +2,14 @@ import PropTypes from "prop-types";
 import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
-const CloseButton = ({ handleClose, dataTestid }) => {
+const CloseButton = ({ handleClose, dataTestid, className }) => {
   return (
-    <IconButton size="large" onClick={handleClose} data-testid={dataTestid}>
+    <IconButton
+      size="large"
+      onClick={handleClose}
+      data-testid={dataTestid}
+      className={className}
+    >
       <CloseIcon />
     </IconButton>
   );
@@ -13,6 +18,7 @@ const CloseButton = ({ handleClose, dataTestid }) => {
 CloseButton.propTypes = {
   handleClose: PropTypes.func,
   dataTestid: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default CloseButton;
