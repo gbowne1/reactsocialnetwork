2d6ff155cd6c8f507bf9b491e65464ba8fe3aad1
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2023-07-20 20:29:56 +0530

    Change /src/index.js to .jsx and remove Strict Mode

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,13 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 // import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
