e23c893018b80af54fb59c3bc8b1f8d5e1a4806c
Manuel <manuelpinedacabeza@gmail.com>
2023-11-01 15:24:56 +0100

    Added baseUrl to config

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: "http://localhost:3000/",
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        experimentalRunAllSpecs: true,
+        video: false,
     },
-    experimentalRunAllSpecs: true,
-    video: false,
-  },
 });
