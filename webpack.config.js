d698637bf1c34c07266dda2321cabff37e2311b0
Alexia Christelle <christellealexia53@gmail.com>
2025-05-13 13:05:34 +0200

    Add env file in gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -20,4 +20,31 @@ module.exports = {
     react: "React",
     "react-dom": "ReactDOM",
   },
+  rules:[
+         {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: [
+          {
+          //needed to chain sourcemaps.  see: https://webpack.js.org/loaders/source-map-loader/
+          loader: 'source-map-loader',
+           options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+          filterSourceMappingUrl: (url, resourcePath) => {
+           //  console.log({ url, resourcePath }) example:
+           // {
+          //  url: 'index.js.map',
+          //  resourcePath: '/repos/xlib-wsl/common/temp/node_modules/.pnpm/https-proxy-agent@5.0.0/node_modules/https-proxy-agent/dist/index.js'
+           // }
+
+          if (/.*\/node_modules\/.*/.test(resourcePath)) {
+          return false
+          }
+          return true
+          }
+
+          }
+          }],
+          },
+         ]
 };
